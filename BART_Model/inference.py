1c79f54051ac3c163e5fe3bc354a33d702c95fdb
mcao610 <c_meng@outlook.com>
2020-02-26 19:37:34 -0500

    create inference file

--- BART_Model/inference.py
+++ BART_Model/inference.py
@@ -1,46 +1,58 @@
 import torch
+import argparse
 from fairseq.models.bart import BARTModel
 
-MODEL_PATH='~/scratch/BART_models/checkpoints_iter'
-DATA_PATH='~/scratch/summarization/cnn_dm/iterative_files/cnn_dm-bin/'
 
+def main(args):
+    print(args.checkpoint_path)
+    print(args.data_path)
+    bart = BARTModel.from_pretrained(
+        args.checkpoint_path,
+        checkpoint_file='checkpoint_best.pt',
+        data_name_or_path=args.data_path
+    )
+    print('- model loaded.')
 
-bart = BARTModel.from_pretrained(
-    MODEL_PATH,
-    checkpoint_file='checkpoint_best.pt',
-    data_name_or_path=DATA_PATH
-)
-print('- model loaded.')
+    bart.cuda()
+    bart.eval()
+    bart.half()
+    bart.model = nn.DataParallel(bart.model)
+    count = 1
+    bsz = 16
 
-bart.cuda()
-bart.eval()
-bart.half()
-bart.model = nn.DataParallel(bart.model)
-count = 1
-bsz = 16
+    print('- batch size: {}'.format(bsz))
 
-print('- batch size: {}'.format(bsz))
+    with open(args.test_path) as source, open(args.output_file, 'w') as fout:
+        sline = source.readline().strip()
+        slines = [sline]
+        for sline in source:
+            if count % bsz == 0:
+                with torch.no_grad():
+                    hypotheses_batch = bart.sample(slines, beam=4, lenpen=2.0, max_len_b=140, min_len=55, no_repeat_ngram_size=3)
+                for hypothesis in hypotheses_batch:
+                    fout.write(hypothesis + '\n')
+                    fout.flush()
+                slines = []
 
-TEST_PATH='~/scratch/summarization/cnn_dm/iterative_files/test.source'
-OUTPUT_FILE='iter_preds.hypo'
+            slines.append(sline.strip())
+            count += 1
 
-with open(source_file) as source, open(OUTPUT_FILE, 'w') as fout:
-    sline = source.readline().strip()
-    slines = [sline]
-    for sline in source:
-        if count % bsz == 0:
-            with torch.no_grad():
-                hypotheses_batch = bart.sample(slines, beam=4, lenpen=2.0, max_len_b=140, min_len=55, no_repeat_ngram_size=3)
+        if slines != []:
+            hypotheses_batch = bart.sample(slines, beam=4, lenpen=2.0, max_len_b=140, min_len=55, no_repeat_ngram_size=3)
             for hypothesis in hypotheses_batch:
                 fout.write(hypothesis + '\n')
                 fout.flush()
-            slines = []
 
-        slines.append(sline.strip())
-        count += 1
+    # DATA_PATH='~/scratch/summarization/cnn_dm/iterative_files/cnn_dm-bin/'
+    # MODEL_PATH='~/scratch/BART_models/checkpoints_iter'
+    # TEST_PATH='~/scratch/summarization/cnn_dm/iterative_files/test.source'
 
-    if slines != []:
-        hypotheses_batch = bart.sample(slines, beam=4, lenpen=2.0, max_len_b=140, min_len=55, no_repeat_ngram_size=3)
-        for hypothesis in hypotheses_batch:
-            fout.write(hypothesis + '\n')
-            fout.flush()
+
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument("--checkpoint_path", type=str, help="checkpoint directory.")
+    PARSER.add_argument("--data_path", type=str, help="train, val or test.")
+    PARSER.add_argument("--test_path", type=str, help="train, val or test.")
+    PARSER.add_argument("--output_file", type=str, default='test.hypo', help="train, val or test.")
+    ARGS = PARSER.parse_args()
+    main(ARGS)
