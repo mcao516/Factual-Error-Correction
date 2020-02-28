bee75db225dd4275274f8558fc4fe4bc3d7613d2
mcao610 <c_meng@outlook.com>
2020-02-28 16:38:38 -0500

    change create data file

--- datasets/cnn_dm/all_corruptions/create_training_data.py
+++ datasets/cnn_dm/all_corruptions/create_training_data.py
@@ -2,83 +2,67 @@ import json
 import random
 import argparse
 
-
 SEP_TOKEN = '</s>'
 
 
-def create_uniform(data_dict, source_file, target_file, corruption_types):
-    """Create uniform data. Half clean summaries and half sampling from other types.
-
-    Arguments:
-        data_dict -- {id: {'text': '', 'clean': '', 'numswp': ''}}
-        source_file -- str, source file path
-        target_file -- str, target file path
-        corruption_types -- [description]
+def ctype_sampler_1(data):
+    """50% probability sample a clean summary; 50% uniformly sample a corrupted summary.
     """
-    sampled_type_counts = {t: 0 for t in corruption_types}
-
-    with open(source_file, 'w', encoding='utf-8') as sf, open(target_file, 'w', encoding='utf-8') as tf:
-        for id in data_dict:
-            doc_text = data_dict[id]['text']
-            summary = data_dict[id]['clean']
-
-            # 50% chance sample corrupted summaries
-            if id % 2 == 0:
-                sample_type = 'clean'
-            else:
-                types = list(data_dict[id].keys())
-                types.remove('text')
-                if len(types) >= 2:
-                    types.remove('clean')
-                sample_type = random.sample(types, 1)[0]
-
-            corrputed = data_dict[id][sample_type]
-            sampled_type_counts[sample_type] += 1
-
-            sf.write(corrputed + ' {} '.format(SEP_TOKEN) + doc_text + '\n')
-            sf.flush()
-            tf.write(summary + '\n')
-            tf.flush()
-
-    print('Create uniform data:')
-    print(sampled_type_counts)
-
-
-def create_mixed(data_dict, source_file, target_file, corruption_types):
-    """Create mixed data. For each summary, use one clean sample and one corruption sample.
-
-    Arguments:
-        data_dict -- {id: {'text': '', 'clean': '', 'numswp': ''}}
-        source_file -- str, source file path
-        target_file -- str, target file path
-        corruption_types -- [description]
-    """
-    sampled_type_counts = {t: 0 for t in corruption_types}
-
-    with open(source_file, 'w', encoding='utf-8') as sf, open(target_file, 'w', encoding='utf-8') as tf:
-        for id in data_dict:
-            doc_text = data_dict[id]['text']
-            summary = data_dict[id]['clean']
-
-            types = list(data_dict[id].keys())
-            types.remove('text')
-            if len(types) <= 1:
-                continue
+    if random.random() < 0.5:
+        sample_type = 'clean'
+    else:
+        types = list(data.keys())
+        types.remove('text')
+        if len(types) >= 2:
             types.remove('clean')
-            sample_type = random.sample(types, 1)[0]
+        sample_type = random.sample(types, 1)[0]
+    return [sample_type]
 
-            corrputed = data_dict[id][sample_type]
-            sampled_type_counts['clean'] += 1
-            sampled_type_counts[sample_type] += 1
 
-            sf.write(corrputed + ' {} '.format(SEP_TOKEN) + doc_text + '\n')
-            sf.write(summary + ' {} '.format(SEP_TOKEN) + doc_text + '\n')
-            sf.flush()
-            tf.write(summary + '\n')
-            tf.write(summary + '\n')
-            tf.flush()
+def ctype_sampler_2(data):
+    """For each data sample, sample a clean summary and a corrupted one.
+    """
+    types = list(data.keys())
+    types.remove('text')
 
-    print('Create mixed data:')
+    samples = ['clean']
+    if len(types) > 1:
+        types.remove('clean')
+        samples.append(random.sample(types, 1)[0])
+    return samples
+
+
+def create_training_data(data_dict, source_file, target_file, corruption_types, duplicate=False):
+    """Create training data. Half clean summaries and half sampling from other types.
+
+    Arguments:
+        data_dict -- {id: {'text': '', 'clean': '', 'numswp': ''}}
+        source_file -- str, source file path
+        target_file -- str, target file path
+        corruption_types -- list of types
+    """
+    sampled_type_counts = {t: 0 for t in corruption_types}
+
+    with open(source_file, 'w', encoding='utf-8') as sf, open(target_file, 'w', encoding='utf-8') as tf:
+        for id in data_dict:
+            doc_text = data_dict[id]['text']
+            summary = data_dict[id]['clean']
+
+            if duplicate:
+                sampled_types = ctype_sampler_2(data_dict[id])
+            else:
+                sampled_types = ctype_sampler_1(data_dict[id])
+
+            for st in sampled_types:
+                corrputed = data_dict[id][st]
+                sampled_type_counts[st] += 1
+
+                sf.write(corrputed + ' {} '.format(SEP_TOKEN) + doc_text + '\n')
+                sf.flush()
+                tf.write(summary + '\n')
+                tf.flush()
+
+    print('Created dataset:')
     print(sampled_type_counts)
 
 
@@ -107,19 +91,24 @@ def main(args):
     print(corruption_types)
 
     data_dict, type_counts = read_data(file_type, corruption_types)
-    print('Data size:')
-    print(len(data_dict))
-    print('Total corruption counts:')
+    print('Dataset size: {}'.format(len(data_dict)))
+    print('All corruption counts:')
     print(type_counts)
 
-    create_uniform(data_dict, file_type + '-uni.source', file_type + '-uni.target', corruption_types)
-    create_mixed(data_dict, file_type + '-mixed.source', file_type + '-mixed.target', corruption_types)
+    if args.duplicate:
+        create_training_data(data_dict, args.source_fout, args.target_fout, corruption_types, True)
+    else:
+        create_training_data(data_dict, args.source_fout, args.target_fout, corruption_types, False)
 
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser()
     PARSER.add_argument("--data_type", type=str, help="train, val or test.")
     PARSER.add_argument("--corruption_types", type=str, nargs="+", default=(), help="")
+    PARSER.add_argument("--source_fout", type=str, help="")
+    PARSER.add_argument("--target_fout", type=str, help="")
+    PARSER.add_argument('--duplicate', action='store_true')
+
     # clean dateswp entswp negation numswp pronoun
     ARGS = PARSER.parse_args()
     main(ARGS)
