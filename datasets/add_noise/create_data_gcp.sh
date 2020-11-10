50f04f00b48691487d3e0846ef47cc97f6df6d0e
mcao610 <c_meng@outlook.com>
2020-11-10 17:09:53 -0500

    delete some jupyter notebooks

--- datasets/add_noise/create_data_gcp.sh
+++
@@ -1,3 +0,0 @@
-FILE_TYPE=train
-
-nohup python create_data.py --source_file ../cnn_dm/fairseq_files/$FILE_TYPE.source --target_file ../cnn_dm/fairseq_files/$FILE_TYPE.target --augmentations backtranslation entity_swap pronoun_swap date_swap number_swap negation --save_intermediate &
